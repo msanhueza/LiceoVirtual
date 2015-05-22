bb892840135a0da02aa2f1a16e98dab761858c62
msanhuezal <msanhuezal@hotmail.cl>
2015-05-22 02:49:13 -0300

    Pregunta y PreguntaSolucion Items

---
+++ LiceoVirtual/LiceoVirtual/TablasItem/PreguntaItem.cs
@@ -0,0 +1,18 @@
+﻿public class PreguntaItem
+{
+
+
+	public PreguntaItem(int ID, string pregunta, int nivel){
+		this.ID= ID;
+		this.pregunta = pregunta;
+		this.nivel = nivel;
+	}
+
+	public int ID { get; set; }
+
+	public string pregunta { get; set; }
+
+	public int nivel { get; set; }
+
+}
+
