941af570c21f2d83daef506d28f3e163f2e0bc28
Francisco H <francisco@MacBook-de-Francisco.local>
2015-07-07 13:08:06 -0300

    merge conflicts

--- LiceoVirtual/LiceoVirtual/Resources/Resource.designer.cs
+++ LiceoVirtual/LiceoVirtual/Resources/Resource.designer.cs
@@ -171,196 +171,187 @@ namespace LiceoVirtual
 			public const int back_arrow = 2130837507;
 			
 			// aapt resource value: 0x7f020004
-			public const int baseLogin = 2130837508;
+			public const int biology = 2130837508;
 			
 			// aapt resource value: 0x7f020005
-			public const int biology = 2130837509;
+			public const int buttonshape = 2130837509;
 			
 			// aapt resource value: 0x7f020006
-			public const int buttonshape = 2130837510;
+			public const int candado = 2130837510;
 			
 			// aapt resource value: 0x7f020007
-			public const int candado = 2130837511;
+			public const int chat_bubble_gray = 2130837511;
 			
 			// aapt resource value: 0x7f020008
-			public const int chat_bubble_gray = 2130837512;
+			public const int chat_bubble_green = 2130837512;
 			
 			// aapt resource value: 0x7f020009
-			public const int chat_bubble_green = 2130837513;
+			public const int editext_bg_gray = 2130837513;
 			
 			// aapt resource value: 0x7f02000a
-			public const int correct = 2130837514;
+			public const int edittext_bg = 2130837514;
 			
 			// aapt resource value: 0x7f02000b
-			public const int editext_bg_gray = 2130837515;
+			public const int error = 2130837515;
 			
 			// aapt resource value: 0x7f02000c
-			public const int edittext_bg = 2130837516;
+			public const int felicitaciones = 2130837516;
 			
 			// aapt resource value: 0x7f02000d
-			public const int error = 2130837517;
+			public const int green_btn = 2130837517;
 			
 			// aapt resource value: 0x7f02000e
-			public const int felicitaciones = 2130837518;
+			public const int ic_action_overflow = 2130837518;
 			
 			// aapt resource value: 0x7f02000f
-			public const int green_btn = 2130837519;
+			public const int ic_compose = 2130837519;
 			
 			// aapt resource value: 0x7f020010
-			public const int ic_action_overflow = 2130837520;
+			public const int ic_launcher = 2130837520;
 			
 			// aapt resource value: 0x7f020011
-			public const int ic_compose = 2130837521;
+			public const int ic_offline = 2130837521;
 			
 			// aapt resource value: 0x7f020012
-			public const int ic_launcher = 2130837522;
+			public const int ic_online = 2130837522;
 			
 			// aapt resource value: 0x7f020013
-			public const int ic_offline = 2130837523;
+			public const int ic_pwd = 2130837523;
 			
 			// aapt resource value: 0x7f020014
-			public const int ic_online = 2130837524;
+			public const int ic_send = 2130837524;
 			
 			// aapt resource value: 0x7f020015
-			public const int ic_pwd = 2130837525;
+			public const int ic_username = 2130837525;
 			
 			// aapt resource value: 0x7f020016
-			public const int ic_send = 2130837526;
+			public const int Icon = 2130837526;
 			
 			// aapt resource value: 0x7f020017
-			public const int ic_username = 2130837527;
+			public const int jugar = 2130837527;
 			
 			// aapt resource value: 0x7f020018
-			public const int Icon = 2130837528;
+			public const int login_ic = 2130837528;
 			
 			// aapt resource value: 0x7f020019
-			public const int incorrect = 2130837529;
+			public const int LogoBlanco = 2130837529;
 			
 			// aapt resource value: 0x7f02001a
-			public const int jugar = 2130837530;
+			public const int logoLogin = 2130837530;
 			
 			// aapt resource value: 0x7f02001b
-			public const int login_ic = 2130837531;
+			public const int LVBackground = 2130837531;
 			
 			// aapt resource value: 0x7f02001c
-			public const int LogoBlanco = 2130837532;
+			public const int LVBackgroundMenu = 2130837532;
 			
 			// aapt resource value: 0x7f02001d
-			public const int logoLogin = 2130837533;
+			public const int LVBackgroundResultado = 2130837533;
 			
 			// aapt resource value: 0x7f02001e
-			public const int LVBackground = 2130837534;
+			public const int LVBlockedButton = 2130837534;
 			
 			// aapt resource value: 0x7f02001f
-			public const int LVBackgroundMenu = 2130837535;
+			public const int LVButton = 2130837535;
 			
 			// aapt resource value: 0x7f020020
-			public const int LVBackgroundResultado = 2130837536;
+			public const int LVMenuBtn = 2130837536;
 			
 			// aapt resource value: 0x7f020021
-			public const int LVBlockedButton = 2130837537;
+			public const int LVMsg = 2130837537;
 			
 			// aapt resource value: 0x7f020022
-			public const int LVButton = 2130837538;
+			public const int LVProgress = 2130837538;
 			
 			// aapt resource value: 0x7f020023
-			public const int LVMenuBtn = 2130837539;
+			public const int n1_1 = 2130837539;
 			
 			// aapt resource value: 0x7f020024
-			public const int LVMsg = 2130837540;
+			public const int n1_10 = 2130837540;
 			
 			// aapt resource value: 0x7f020025
-			public const int LVProgress = 2130837541;
+			public const int n1_13 = 2130837541;
 			
 			// aapt resource value: 0x7f020026
-			public const int n1_1 = 2130837542;
+			public const int n1_14 = 2130837542;
 			
 			// aapt resource value: 0x7f020027
-			public const int n1_10 = 2130837543;
+			public const int n1_15 = 2130837543;
 			
 			// aapt resource value: 0x7f020028
-			public const int n1_13 = 2130837544;
+			public const int n1_17 = 2130837544;
 			
 			// aapt resource value: 0x7f020029
-			public const int n1_14 = 2130837545;
+			public const int n1_18 = 2130837545;
 			
 			// aapt resource value: 0x7f02002a
-			public const int n1_15 = 2130837546;
+			public const int n1_19 = 2130837546;
 			
 			// aapt resource value: 0x7f02002b
-			public const int n1_17 = 2130837547;
+			public const int n1_2 = 2130837547;
 			
 			// aapt resource value: 0x7f02002c
-			public const int n1_18 = 2130837548;
+			public const int n1_20 = 2130837548;
 			
 			// aapt resource value: 0x7f02002d
-			public const int n1_19 = 2130837549;
+			public const int n1_23 = 2130837549;
 			
 			// aapt resource value: 0x7f02002e
-			public const int n1_2 = 2130837550;
+			public const int n1_26 = 2130837550;
 			
 			// aapt resource value: 0x7f02002f
-			public const int n1_20 = 2130837551;
+			public const int n1_3 = 2130837551;
 			
 			// aapt resource value: 0x7f020030
-			public const int n1_23 = 2130837552;
+			public const int n1_4 = 2130837552;
 			
 			// aapt resource value: 0x7f020031
-			public const int n1_26 = 2130837553;
+			public const int n1_5 = 2130837553;
 			
 			// aapt resource value: 0x7f020032
-			public const int n1_3 = 2130837554;
+			public const int n1_6 = 2130837554;
 			
 			// aapt resource value: 0x7f020033
-			public const int n1_4 = 2130837555;
+			public const int n1_7 = 2130837555;
 			
 			// aapt resource value: 0x7f020034
-			public const int n1_5 = 2130837556;
+			public const int n1_8 = 2130837556;
 			
 			// aapt resource value: 0x7f020035
-			public const int n1_6 = 2130837557;
+			public const int n1_9 = 2130837557;
 			
 			// aapt resource value: 0x7f020036
-			public const int n1_7 = 2130837558;
+			public const int nota = 2130837558;
 			
 			// aapt resource value: 0x7f020037
-			public const int n1_8 = 2130837559;
+			public const int pizarron = 2130837559;
 			
 			// aapt resource value: 0x7f020038
-			public const int n1_9 = 2130837560;
+			public const int punto = 2130837560;
 			
 			// aapt resource value: 0x7f020039
-			public const int nota = 2130837561;
+			public const int puntos = 2130837561;
 			
 			// aapt resource value: 0x7f02003a
-			public const int pizarron = 2130837562;
+			public const int respuesta_correcta = 2130837562;
 			
 			// aapt resource value: 0x7f02003b
-			public const int punto = 2130837563;
+			public const int respuesta_incorrecta = 2130837563;
 			
 			// aapt resource value: 0x7f02003c
-			public const int puntos = 2130837564;
+			public const int splash = 2130837564;
 			
 			// aapt resource value: 0x7f02003d
-			public const int respuesta_correcta = 2130837565;
+			public const int student = 2130837565;
 			
 			// aapt resource value: 0x7f02003e
-			public const int respuesta_incorrecta = 2130837566;
+			public const int student_sad = 2130837566;
 			
 			// aapt resource value: 0x7f02003f
-			public const int splash = 2130837567;
+			public const int user_chat1 = 2130837567;
 			
 			// aapt resource value: 0x7f020040
-			public const int student = 2130837568;
-			
-			// aapt resource value: 0x7f020041
-			public const int student_sad = 2130837569;
-			
-			// aapt resource value: 0x7f020042
-			public const int user_chat1 = 2130837570;
-			
-			// aapt resource value: 0x7f020043
-			public const int user_chat2 = 2130837571;
+			public const int user_chat2 = 2130837568;
 			
 			static Drawable()
 			{
